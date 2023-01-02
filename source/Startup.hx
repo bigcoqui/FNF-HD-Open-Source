bc97b5d34305315fc1188bc987ad960c744c5781
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2023-01-02 09:30:51 -0300

    update Startup.hx

--- source/Startup.hx
+++ source/Startup.hx
@@ -12,7 +12,6 @@ import flixel.math.FlxRect;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import flixel.text.FlxText;
-import sys.FileSystem;
 import flixel.FlxG;
 import haxe.ds.Map;
 
@@ -20,26 +19,14 @@ using StringTools;
 
 class Startup extends MusicBeatState
 {
-
-    /*final songPreload:Array<String> =   ["Tutorial", 
-    "Bopeebo", "Fresh", "Dadbattle", 
-    "Spookeez", "South", "Monster",
-    "Pico", "Philly", "Blammed", 
-    "Satin-Panties", "High", "Milf", 
-    "Cocoa", "Eggnog", "Winter-Horrorland", 
-	"Breaking-Point",
-    "Green-Hill", "Racing", "Boom", "Happy-Time"];*/
-
     public static var atlasFrames:Map<String, FlxGraphic> = new Map<String, FlxGraphic>();
     var musicDone:Bool = false;
     var atlasDone:Bool = false;
-   
-	//var Cacher:GraphicsCacher;
+
     var loadingText:FlxText;
 
 	override function create()
 	{
-
         FlxG.mouse.visible = false;
         var loadingBG = new FlxSprite(0.0).loadGraphic(Paths.image('loadingscreen'));
         loadingBG.antialiasing = true;
@@ -50,10 +37,6 @@ class Startup extends MusicBeatState
         loadingText.setFormat("assets/fonts/vcr.ttf", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         add(loadingText);
 
-        //new FlxTimer().start(1.1, function(tmr:FlxTimer)
-        //{
-        //    FlxG.sound.play("assets/sounds/splashSound.ogg");   
-        //});
         sys.thread.Thread.create(() -> {
             preloadAtlas();
         });
@@ -61,29 +44,22 @@ class Startup extends MusicBeatState
             preloadMusic();
         });
         super.create();
-        
     }
 
     override function update(elapsed) 
     {
-        
-      
        if (atlasDone && musicDone)
         {
         loadingText.text = "Done!";
         FlxG.sound.play(Paths.sound('confirmMenu'));
         FlxG.switchState(new TitleState());
         }
-            
         super.update(elapsed);
-
     }
-
-  
 
     function preloadMusic():Void{
         var music = [];
-        for (i in FileSystem.readDirectory(FileSystem.absolutePath("assets/songs")))
+        for (i in HSys.readDirectory("assets/songs"))
             {
                 music.push(i);
             }
@@ -91,40 +67,27 @@ class Startup extends MusicBeatState
         for (i in music)
             {
                 FlxG.sound.cache(Paths.inst(i));
-                //FlxG.sound.cache(Paths.voices(i));
                 trace("cached " + i);
-                
             }
         musicDone = true;
     }
-    
-    /*function preloadMusic():Void{
-        for(x in songPreload){
-			FlxG.sound.cache(Paths.inst(x));
-			trace("Chached " + x);
-		}
-        musicDone = true;
-    }*/
 
     function preloadAtlas():Void{
     var atlasList = [];
-    for (i in FileSystem.readDirectory(FileSystem.absolutePath('assets/images/TextureAtlas')))
+    for (i in HSys.readDirectory('assets/images/TextureAtlas'))
         {
         atlasList.push(i);
 		trace(i);
         }
-    
+
     for (i in atlasList)
         {
         var bitmap = FlxGraphic.fromAssetKey(Paths.image('TextureAtlas/' + i + '/spritemap'));
         var testSprite = new FlxSprite(0,0,bitmap);
-        //add(testSprite);
         bitmap.persist = true;
         bitmap.destroyOnNoUse = false;
         atlasFrames.set(i,bitmap);
         }
-      
         atlasDone = true;
-        
     }
 }
